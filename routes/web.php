5bd093c9e9aa3088c254be24873e557929db9940
Alriskhandy <alriskhandy19@gmail.com>
2025-07-10 16:41:29 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [FrontendController::class, 'index'])->name('beranda');
+Route::get('/peta-gis', [FrontendController::class, 'showMap'])->name('tampil.peta');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
