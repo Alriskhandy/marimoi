13ec3dc5e295195249f1d5409d4089fc060de88e
Alriskhandy <alriskhandy19@gmail.com>
2025-07-31 00:18:58 +0900

    tambah gambar dummy di detail lokasi

--- resources/views/frontend/pages/detail.blade.php
+++ resources/views/frontend/pages/detail.blade.php
@@ -8,35 +8,6 @@
     <!-- Hero Section -->
     @include('frontend.partials.nav-map')
 
-    {{-- <!-- Hero Section -->
-    <section id="hero" class="hero section dark-background position-relative">
-
-        <img src="{{ asset('frontend/img/hero2.png') }}" alt="" class="hero-bg" data-aos="fade-in">
-
-        <div class="overlay-dark position-absolute top-0 start-0 w-100 h-100"
-            style="z-index: 1; background-color: rgba(0, 0, 0, 0.5);"></div>
-
-        <div class="container position-relative" style="z-index: 2;">
-            <div class="row gy-4 d-flex justify-content-center">
-                <div class="col-lg-10 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                    <h2 class="text-center text-white" data-aos="fade-up">SISTEM INFORMASI MANAJEMEN AKSELERASI
-                        INFRASTRUKTUR UNTUK MONITORING DAN INTEGRASI WILAYAH</h2>
-                    <p class="text-white text-center" data-aos="fade-up" data-aos-delay="100">Sistem digital terpadu
-                        berbasis web dan mobile yang dikembangkan untuk mendukung perencanaan, pelaksanaan, pemantauan, dan
-                        evaluasi pembangunan infrastruktur daerah secara lebih efektif, partisipatif, dan terintegrasi.
-                        Sistem ini menyasar penguatan sinergi lintas sektor dan wilayah dalam mendukung pembangunan wilayah
-                        Provinsi Maluku Utara.</p>
-                </div>
-
-                <div class="col-lg-5 order-1 order-lg-2 hero-img" data-aos="zoom-out">
-                    <img src="assets/img/hero-img.svg" class="img-fluid mb-3 mb-lg-0" alt="">
-                </div>
-
-            </div>
-        </div>
-
-    </section><!-- /Hero Section --> --}}
-
     <!-- Detail Section -->
     <section class="section py-4">
         <!-- Section Title -->
@@ -47,16 +18,28 @@
 
         <div class="container">
             @if (isset($project))
-                <div class="row gy-4">
-
+                <div class="row gy-4 align-items-stretch">
                     <!-- Detail Map -->
-                    <div class="col-md-4 col-lg-6">
-                        <div id="map-detail" class="mb-4" style="border:0; width: 100%; height: 400px; min-height: 300px;"
-                            data-aos="fade-up" data-aos-delay="200"></div>
-                    </div><!-- End Detail Map -->
+                    <div class="col-md-6">
+                        <div id="map-detail" class="rounded shadow-sm h-100"
+                            style="width: 100%; min-height: 300px; max-height: 400px; height: 100%;" data-aos="fade-up"
+                            data-aos-delay="200">
+                        </div>
+                    </div>
 
-                    <div class="col-md-8 col-lg-6">
+                    <!-- Gambar -->
+                    <div class="col-md-6">
+                        <div class="h-100">
+                            <img src="{{ asset('frontend/img/kantor-gub-malut.jpeg') }}" alt="Kantor Gubernur Malut"
+                                class="img-fluid rounded shadow-sm w-100"
+                                style="height: 100%; object-fit: cover; min-height: 300px; max-height: 400px;">
+                        </div>
+                    </div>
+                </div>
 
+                <!-- Deskripsi -->
+                <div class="row gy-4 mt-4">
+                    <div class="col-md-12">
                         <table class="table table-bordered">
                             <tbody>
                                 <tr>
