b5ebd8e97f0a974bb320403ce5952b0297a2fa6d
Alriskhandy <alriskhandy19@gmail.com>
2025-09-07 23:06:14 +0900

    ganti logo

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -18,7 +18,7 @@
         <div class="row">
             <div class="col-lg-4 col-md-6 mb-4">
                 <h5>
-                    <img width="50" src="{{ asset('frontend/img/logo.webp') }}" alt="Logo MARIMOI" />
+                    <img width="50" src="{{ asset('frontend/img/logo/logo-white.png') }}" alt="Logo MARIMOI" />
                     MARIMOI
                 </h5>
                 <p class="mb-4">
--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg">
     <div class="container">
         <a class="navbar-brand" href="/">
-            <img width="40" src="{{ asset('frontend/img/logo.webp') }}" alt="Logo Bappeda" />
+            <img width="40" src="{{ asset('frontend/img/logo/logo-dark.png') }}" alt="Logo Bappeda" />
             <span>MARIMOI</span>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
